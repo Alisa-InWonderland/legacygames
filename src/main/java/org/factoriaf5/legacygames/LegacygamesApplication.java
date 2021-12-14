db03fe993465ad286f8843b1753f05c476dc72ea
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-12-14 14:11:58 +0100

    Initial commit

---
+++ src/main/java/org/factoriaf5/legacygames/LegacygamesApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.legacygames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacygamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacygamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/legacygames/LegacygamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.legacygames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacygamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
